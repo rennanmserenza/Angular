96d78535477e911c9d7bab8451f6af986ed38aa5
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-08-10 14:06:28 -0400

    [New] Criação de componentes para teste do ng lint

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { TesteComponentComponent } from './teste-component/teste-component.component';
 
 @NgModule({
-	declarations: [AppComponent, DiretivaNgifComponent],
+	declarations: [AppComponent, DiretivaNgifComponent, TesteComponentComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
