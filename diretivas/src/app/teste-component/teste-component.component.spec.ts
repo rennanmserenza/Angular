60fa3184e75ee7759361b348c3cf3b97e69fbfe3
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-08-10 14:14:31 -0400

    [New] alterações para execução do comando ng test.

--- diretivas/src/app/teste-component/teste-component.component.spec.ts
+++ diretivas/src/app/teste-component/teste-component.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TesteComponentComponent } from './teste-component.component';
 
 describe('TesteComponentComponent', () => {
-  let component: TesteComponentComponent;
-  let fixture: ComponentFixture<TesteComponentComponent>;
+	let component: TesteComponentComponent;
+	let fixture: ComponentFixture<TesteComponentComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TesteComponentComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [TesteComponentComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(TesteComponentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(TesteComponentComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- diretivas/src/app/teste-component/teste-component.component.ts
+++ diretivas/src/app/teste-component/teste-component.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-teste-component',
-  templateUrl: './teste-component.component.html',
-  styleUrls: ['./teste-component.component.css']
+	selector: 'app-teste-component',
+	templateUrl: './teste-component.component.html',
+	styleUrls: ['./teste-component.component.css'],
 })
 export class TesteComponentComponent implements OnInit {
+	minhaVariavel: string = '';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		console.log('');
+	}
 }
