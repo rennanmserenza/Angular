27f18af586c84b14fe750f8b56588c211aa1c967
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-08-11 09:15:10 -0400

    [New] recriação do módulo de diretivas

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
-import { TesteComponentComponent } from './teste-component/teste-component.component';
 
 @NgModule({
-	declarations: [AppComponent, DiretivaNgifComponent, TesteComponentComponent],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
