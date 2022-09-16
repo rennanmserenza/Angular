04f91baa66779c131a75cc3e84f8640ef3981745
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-09-16 14:28:33 -0400

    [UPDATE] Conteúdo da aula sobre ng-content

--- diretivas/src/app/ng-content/ng-content.component.ts
+++ diretivas/src/app/ng-content/ng-content.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-ng-content',
-  templateUrl: './ng-content.component.html',
-  styleUrls: ['./ng-content.component.css']
+	selector: 'app-ng-content',
+	templateUrl: './ng-content.component.html',
+	styleUrls: ['./ng-content.component.css'],
 })
-export class NgContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NgContentComponent {
+	constructor() {}
 }
