8a8d2ec00e6e88d1b9169b3051a53332b1085dcd
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-09-19 08:34:28 -0400

    [UPDATE] Métodos de HostListner e HostBinding.

--- diretivas/src/app/shared/highlight-mouse.directive.ts
+++ diretivas/src/app/shared/highlight-mouse.directive.ts
@@ -1,10 +1,45 @@
-import { Directive } from '@angular/core';
+import { style } from '@angular/animations';
+import {
+	Directive,
+	HostListener,
+	HostBinding,
+	ElementRef,
+	Renderer2,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightMouse]'
+	selector: '[appHighlightMouse]',
 })
 export class HighlightMouseDirective {
+	@HostListener('mouseenter')
+	onMouseOver() {
+		// 	this.renderer.setStyle(
+		// 		this.elementRef.nativeElement,
+		// 		'background-color',
+		// 		'blue'
+		// 	);
+		this.backgroundColor = 'blue';
+	}
 
-  constructor() { }
+	@HostListener('mouseleave')
+	onMouseLeave() {
+		// 	this.renderer.setStyle(
+		// 		this.elementRef.nativeElement,
+		// 		'background-color',
+		// 		'white'
+		// 	);
+		this.backgroundColor = 'white';
+	}
 
+	//@HostBinding('style.backgroundColor') backgroundColor: string;
+	@HostBinding('style.backgroundColor') get SetColor() {
+		return this.backgroundColor;
+	}
+
+	private backgroundColor: string;
+
+	constructor() {
+		//private elementRef: ElementRef, private renderer: Renderer2
+		this.backgroundColor = '';
+	}
 }
