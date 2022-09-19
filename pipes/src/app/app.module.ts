85cbf2bfaa0710a813bca133d7c517d2a9199b16
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-09-19 14:40:31 -0400

    [NEW] Criação componente exemplos-pipes.

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, ExemplosPipesComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
