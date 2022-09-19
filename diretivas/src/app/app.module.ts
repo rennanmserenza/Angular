df3b70cdaae9174b549be06f7b57e6185946721d
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-09-19 07:48:18 -0400

    [NEW] Criação diretiva HighLight-Mouse.

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
 import { NgContentComponent } from './ng-content/ng-content.component';
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 
 @NgModule({
 	declarations: [
@@ -27,6 +28,7 @@ import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretiv
   NgContentComponent,
   FundoAmareloDirective,
   DiretivasCustomizadasComponent,
+  HighlightMouseDirective,
 	],
 	imports: [BrowserModule, NgbModule, FormsModule],
 	providers: [],
