c2fbcf1a2e84ef4f26e5fd4b81e92fc8bfd40355
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-10-04 14:39:22 -0400

    [UPDATE] instalação pacotes angular 2 materialize.

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, AppRoutingModule],
