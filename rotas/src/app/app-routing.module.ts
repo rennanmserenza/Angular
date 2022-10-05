872a1d16181ddeff0a8e484ea2750a7a7b7e0bb1
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-10-05 07:49:46 -0400

    [NEW] Criação projeto estudos rotas

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,27 +1,10 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-
-const routes: Routes = [
-	{
-		path: '',
-		component: HomeComponent,
-	},
-	{
-		path: 'login',
-		component: LoginComponent,
-	},
-	{
-		path: 'cursos',
-		component: CursosComponent,
-	},
-];
+const routes: Routes = [];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HomeComponent,
-		LoginComponent,
-		CursosComponent,
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		NgbModule,
-	],
-	providers: [],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
