71dac2387df94302f11dc539cb1c7e72e459d523
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-10-14 11:08:10 -0400

    [UPDATE] Criação de componentes de login e pagina não encontrada.

--- forms/src/app/app-routing.module.ts
+++ forms/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemplateFormComponent } from './template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'templateForm',
+		component: TemplateFormComponent,
+	},
+	{
+		path: '',
+		children: [],
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
+import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { LoginComponent } from './core/login/login.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  TemplateFormComponent
+  TemplateFormComponent,
+  PaginaNaoEncontradaComponent,
+  LoginComponent
 	],
 	imports: [
 		TooltipModule.forRoot(),
