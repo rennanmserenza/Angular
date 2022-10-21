22f17914a1bb5ad53b76e7013747bfa5f2f9fc51
Rennan Mendes Serenza <rennan.mserenza@gmail.com>
2022-10-21 15:56:34 -0400

    [UPDATE] Transformando template drive em data driven, trabalhando validações de tela.

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -19,55 +19,88 @@ export class DataFormComponent implements OnInit {
 	constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
 	ngOnInit(): void {
-		// this.formulario = new FormGroup({
-		// 	nome: new FormControl(null),
-		// 	email: new FormControl(null),
-		// });
-
 		this.formulario = this.formBuilder.group({
 			nome: [
 				null,
 				[Validators.required, Validators.pattern('^[a-zA-Zs]*$')],
 			],
 			email: [null, [Validators.required, Validators.email]],
+			endereco: this.formBuilder.group({
+				cep: [null, [Validators.required]],
+				numero: [null, [Validators.required]],
+				complemento: [null],
+				rua: [null, [Validators.required]],
+				bairro: [null, [Validators.required]],
+				cidade: [null, [Validators.required]],
+				estado: [null, [Validators.required]],
+				uf: [null, [Validators.required]],
+			}),
 		});
 	}
 
-	onSubmit() {
-		console.log(this.formulario);
-		console.log(this.formulario.value);
+	consultaCEP() {
+		let cep = this.formulario.get('endereco.cep')?.value;
 
-		this.http
-			.post(
-				'https://httpbin.org/post',
-				JSON.stringify(this.formulario.value)
-			)
-			.pipe(map((res) => res))
-			.subscribe(
-				(dados) => {
-					console.log(dados);
-					this.resetar();
-				},
-				(error: any) => alert(`Erro: ${error}`)
-			);
+		let cepPesquisa = cep?.replace(/\D/g, '');
 
-		// console.log(this.usuario);
+		if (!this.verificaCampoVazio('endereco.cep')) {
+			let validacep = /^[0-9]{8}$/;
+
+			if (validacep.test(cepPesquisa)) {
+				this.resetaDadosEnderecoForm();
+
+				this.http
+					.get(`https://viacep.com.br/ws/${cepPesquisa}/json/`)
+					.subscribe((dados) => this.consultaEstadoIBGE(dados));
+			}
+		}
 	}
 
-	resetar() {
-		this.formulario.reset();
+	consultaEstadoIBGE(dados: any) {
+		let uf = dados.uf;
+
+		if (uf != null && uf != '' && uf != undefined) {
+			this.http
+				.get(
+					`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}`
+				)
+				.subscribe((nomeEstado: any) =>
+					this.preencherDadosEnderecoForm(dados, nomeEstado)
+				);
+		}
 	}
 
-	verificaValid(campo: any) {
-		console.log(`Campo: ${campo}, valido? ${campo.valid}`);
-		return campo.valid;
+	resetaDadosEnderecoForm() {
+		this.formulario.patchValue({
+			endereco: {
+				rua: null,
+				bairro: null,
+				cidade: null,
+				estado: null,
+				uf: null,
+			},
+		});
 	}
 
-	verificaSubmit(formulario: any) {
-		return this.formulario;
+	preencherDadosEnderecoForm(dados: any, estado: any) {
+		this.formulario.patchValue({
+			endereco: {
+				cep: dados.cep,
+				rua: dados.logradouro,
+				bairro: dados.bairro,
+				cidade: dados.localidade,
+				estado: estado.nome,
+				uf: dados.uf,
+			},
+		});
 	}
 
-	verificaCampoVazio(campo: any) {
+	verificaValid(campo: string) {
+		let compoenente = this.formulario.get(campo);
+		return compoenente?.valid;
+	}
+
+	verificaCampoVazio(campo: string) {
 		let compoenente = this.formulario.get(campo);
 		return compoenente?.value == null || compoenente?.value == '';
 	}
@@ -77,27 +110,73 @@ export class DataFormComponent implements OnInit {
 		return campo.touched;
 	}
 
-	verificaValidAndTouched(campo: any) {
+	verificaValidAndTouched(campo: string) {
 		let compoenente = this.formulario.get(campo);
-		return !this.verificaValid(compoenente) && compoenente?.touched;
+		return !this.verificaValid(campo) && compoenente?.touched;
 	}
 
-	aplicaCssValidacao(campo: any) {
+	aplicaCssValidacao(campo: string) {
 		let compoenente = this.formulario.get(campo);
-		console.log(compoenente);
 		return {
 			'is-invalid':
-				!this.verificaValid(compoenente) &&
+				!this.verificaValid(campo) &&
 				this.verificaSubmitOrTouched(compoenente),
 			'is-valid':
-				this.verificaValid(compoenente) &&
+				this.verificaValid(campo) &&
 				this.verificaSubmitOrTouched(compoenente),
 		};
 	}
 
-	verificaErrors(campo: any, tipo: string) {
+	verificaErrors(campo: string, tipo: string) {
 		let compoenente = this.formulario.get(campo);
-
 		return compoenente?.errors?.[tipo];
+	}
+
+	chamaTextoObrigatorioReadonly(campo: string) {
+		return this.verificaCampoVazio(campo);
+	}
+
+	resetar() {
+		this.formulario.reset();
+	}
+
+	verificaValidacoesForm(formGroup: FormGroup) {
+		Object.keys(formGroup.controls).forEach((campo) => {
+			console.log(campo);
+			const controle = formGroup.get(campo);
+			controle?.markAsTouched();
+
+			if (controle instanceof FormGroup) {
+				this.verificaValidacoesForm(controle);
+			}
+		});
+	}
+
+	onSubmit() {
+		if (this.formulario.valid) {
+			this.http
+				.post(
+					'https://httpbin.org/post',
+					JSON.stringify(this.formulario.value)
+				)
+				.pipe(map((res) => res))
+				.subscribe(
+					(dados) => {
+						console.log(dados);
+						this.resetar();
+					},
+					(error: any) => alert(`Erro: ${error}`)
+				);
+		} else {
+			Object.keys(this.formulario.controls).forEach((campo) => {
+				const controle = this.formulario.get(campo);
+				controle?.markAllAsTouched();
+
+				// validação com touched com recursividade
+				// this.verificaValidacoesForm(this.formulario);
+			});
+		}
+
+		console.log(this.formulario);
 	}
 }
